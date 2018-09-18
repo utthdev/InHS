5724b021069907b16d1d6344ea60a6a179e2c56c
utthdev <utthdev@gmail.com>
2018-09-18 19:11:20 +0700

    add new file

--- src/org/utt/app/opd/OPDPanel.java
+++ src/org/utt/app/opd/OPDPanel.java
@@ -5,14 +5,26 @@ import java.awt.Dimension;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.ImageIcon;
+
 import org.utt.app.common.ObjectData;
 import org.utt.app.common.UserView;
+import org.utt.app.hdfs.MainHDFSOPDPanel;
 
 import com.alee.laf.panel.WebPanel;
+import com.alee.laf.tabbedpane.WebTabbedPane;
 
 public class OPDPanel extends WebPanel implements Observer,UserView{
 	ObjectData oUserInfo;
     int width,height;
+    
+    WebTabbedPane tabbedPane,tabbedPane_in;
+    WebPanel midPanel;
+    
+    MainHDFSOPDPanel mainHDFSOPDPanel;
+    ScanScope scanScope;
+    OPDFormPanel opdFormPanel;
+    
     
     public OPDPanel(ObjectData oUserInfo, int w, int h) {
     	this.oUserInfo=oUserInfo;
@@ -21,9 +33,26 @@ public class OPDPanel extends WebPanel implements Observer,UserView{
         setLayout(new BorderLayout(0, 0));
         setPreferredSize(new Dimension(width, height));
         setBounds(0, 0, width, height);
+        
+        mainHDFSOPDPanel =new MainHDFSOPDPanel(oUserInfo,w,h);
+        scanScope =new ScanScope(oUserInfo,w,h);
+        opdFormPanel =new OPDFormPanel(oUserInfo,w,h);
+        
+        setRight();
+        
     }
     public void update(Observable oObservable, Object oObject) {
         oUserInfo = ((ObjectData) oObservable); // cast
+    }
+    public void setRight(){
+    	midPanel = new WebPanel();
+        midPanel.setLayout(new BorderLayout(0, 0));
+        midPanel.setPreferredSize(new Dimension(width-((width*2)/10), height-140));
+        tabbedPane_in = new WebTabbedPane();
+        midPanel.add(tabbedPane_in, BorderLayout.CENTER);
+        tabbedPane_in.addTab("OPD FORM" ,new ImageIcon(getClass().getClassLoader().getResource("images/indent_out.gif")),opdFormPanel);
+        tabbedPane_in.addTab("EMR",new ImageIcon(getClass().getClassLoader().getResource("images/list_unordered.gif")) ,mainHDFSOPDPanel);
+        
     }
     public void getData() {
     	
