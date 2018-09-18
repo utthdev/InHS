49dea1fbde3d13781c58a4044779c8cca0c64d44
utthdev <utthdev@gmail.com>
2018-09-18 19:25:09 +0700

    edit color

--- src/org/utt/app/opd/OPDPanel.java
+++ src/org/utt/app/opd/OPDPanel.java
@@ -6,12 +6,14 @@ import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.ImageIcon;
+import javax.swing.JTabbedPane;
 
 import org.utt.app.common.ObjectData;
 import org.utt.app.common.UserView;
 import org.utt.app.hdfs.MainHDFSOPDPanel;
 
 import com.alee.laf.panel.WebPanel;
+import com.alee.laf.splitpane.WebSplitPane;
 import com.alee.laf.tabbedpane.WebTabbedPane;
 
 public class OPDPanel extends WebPanel implements Observer,UserView{
@@ -19,7 +21,8 @@ public class OPDPanel extends WebPanel implements Observer,UserView{
     int width,height;
     
     WebTabbedPane tabbedPane,tabbedPane_in;
-    WebPanel midPanel;
+    WebPanel leftPanel,topMainPanel,midPanel;
+    WebSplitPane split,splitR;
     
     MainHDFSOPDPanel mainHDFSOPDPanel;
     ScanScope scanScope;
@@ -34,12 +37,29 @@ public class OPDPanel extends WebPanel implements Observer,UserView{
         setPreferredSize(new Dimension(width, height));
         setBounds(0, 0, width, height);
         
+        split = new WebSplitPane(split.HORIZONTAL_SPLIT);
+        split.setDividerLocation((width*2)/10);
+        splitR = new WebSplitPane(split.VERTICAL_SPLIT);
+        splitR.setDividerLocation(140);
+        
         mainHDFSOPDPanel =new MainHDFSOPDPanel(oUserInfo,w,h);
         scanScope =new ScanScope(oUserInfo,w,h);
         opdFormPanel =new OPDFormPanel(oUserInfo,w,h);
         
         setRight();
         
+        split.setRightComponent(splitR);
+        split.setLeftComponent(leftPanel);
+        split.setOneTouchExpandable(true);
+        splitR.setTopComponent(topMainPanel);
+        splitR.setBottomComponent(midPanel);
+
+        tabbedPane = new WebTabbedPane();
+        tabbedPane.setTabPlacement(JTabbedPane.LEFT);
+
+        tabbedPane.addTab("<html>O<br>P<br>D<br> </html>", null, split, "OPD");
+        add(tabbedPane, BorderLayout.CENTER);
+
     }
     public void update(Observable oObservable, Object oObject) {
         oUserInfo = ((ObjectData) oObservable); // cast
